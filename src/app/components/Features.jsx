8f31e1e7d1d8a4fecbddb72b248ac6875de742aa
dharmesh1811 <dharmeshkatriya512@gmail.com>
2026-03-14 18:41:52 +0530

    card changes

--- src/app/components/Features.jsx
+++ src/app/components/Features.jsx
@@ -79,7 +79,7 @@ export function Features() {
           </p>
         </motion.div>
 
-        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {features.map((feature, index) => (
             <motion.div
               key={feature.title}
